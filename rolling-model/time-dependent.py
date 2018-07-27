fa7bbe736a4bb49038a70a6c0647049428e396ff
Andrew Watson <awatson@math.utah.edu>
2018-07-27 14:47:37 -0600

    Cleaned up code for time dependent problem

--- rolling-model/time-dependent.py
+++ rolling-model/time-dependent.py
@@ -1,14 +1,12 @@
 from constructA import *
-from scipy.sparse import eye
 import matplotlib.pyplot as plt
 
-# Parameters
+# Numerical Parameters
 L, T = 2.0, 0.2
 M, N, time_steps = 100, 100, 500
 z_mesh, th_mesh = np.meshgrid(np.linspace(-L, L, 2*M+1),
                               np.linspace(-np.pi/2, np.pi/2, N+1),
                               indexing='ij')
-z_vec, th_vec = np.ravel(z_mesh, order='F'), np.ravel(th_mesh, order='F')
 m_mesh = np.zeros(shape=(2*M+1, N+1, time_steps+1))
 
 t = np.linspace(0, T, time_steps+1)
@@ -17,6 +15,7 @@ nu = th_mesh[0, 1] - th_mesh[0, 0]
 dt = t[1]-t[0]
 lam = nu/h
 
+# Biological Parameters
 d_prime = .1
 eta = .1
 delta = 3.0
@@ -27,8 +26,6 @@ eta_om = .01
 
 om_f = 20.0
 v_f = (1 + d_prime)*om_f
-
-# A, B, C, D, R = construct_system(M, N, eta, z_vec, th_vec, delta, nu, kap, d_prime)
 
 om = np.zeros(time_steps + 1)
 v = np.zeros(time_steps + 1)
