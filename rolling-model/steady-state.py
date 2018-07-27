1769bf7e402dc680d3c2adb904df40a2f17d0a92
Andrew Watson <awatson@math.utah.edu>
2018-07-27 14:46:49 -0600

    Edited code to make specific for the rolling and sliding case

--- rolling-model/steady-state.py
+++ rolling-model/steady-state.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 # Parameters
 L = 2.0
-M, N = 100, 100
+M, N = 50, 50
 om_max, om_number = 300, 101
+v_max, v_number = 200, 6
 z_mesh, th_mesh = np.meshgrid(np.linspace(-L, L, 2*M+1),
                               np.linspace(-np.pi/2, np.pi/2, N+1),
                               indexing='ij')
@@ -14,7 +15,7 @@ h = z_mesh[1, 0] - z_mesh[0, 0]
 nu = th_mesh[0, 1] - th_mesh[0, 0]
 lam = nu/h
 
-d_prime = 0
+d_prime = .1
 eta = .1
 delta = 3
 kap = 1
@@ -22,13 +23,17 @@ radius = 1
 xi = 1000
 
 omegas = np.linspace(0, om_max, om_number)
-torques = np.zeros(om_number)
+vees = np.linspace(0, v_max, v_number)
+torques, forces = np.zeros(shape=(om_number, v_number)), np.zeros(shape=(om_number, v_number))
 A, B, C, D, R = construct_system(M, N, eta, z_vec, th_vec, delta, nu, kap, d_prime)
 for i in range(om_number):
-    torques[i] = find_torque_roll(A, B, C, D, R, omegas[i], (1+d_prime)*omegas[i],
-                                  lam, nu, h, kap, M, z_mesh, th_mesh, d_prime)[0]
+    for j in range(v_number):
+        torques[i, j], forces[i, j] = \
+            find_torque_roll(A, B, C, D, R, omegas[i], vees[j], lam, nu,
+                             h, kap, M, z_mesh, th_mesh, d_prime)[:-1]
 
-plt.plot(omegas, torques)
+for j in range(v_number):
+    plt.plot(omegas, torques[:,j])
 plt.xlabel('Rotation rate ($\omega$)')
 plt.ylabel('Torque ($\\tau$)')
 plt.show()
