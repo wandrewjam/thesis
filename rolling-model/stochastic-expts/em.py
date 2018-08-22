ed91a9a3788c064381465166fbf1ab49170bf102
Andrew Watson <awatson@math.utah.edu>
2018-08-22 11:55:13 -0600

    Slight corrections to the Euler-Maruyama code

--- rolling-model/stochastic-expts/em.py
+++ rolling-model/stochastic-expts/em.py
@@ -1,7 +1,7 @@
 # Python analog of em.m in Higham, 2001
 # Euler-Maruyama method on linear SDE
 
-# SDE is dW = lam*X dt + mu*X dW, X(0) = Xzero
+# SDE is dX = lam*X dt + mu*X dW, X(0) = Xzero
 #   where lam = 2, mu = 1, and Xzero = 1.
 
 # Discretized Brownian path over [0, 1] has dt = 2**(-8).
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 # np.random.seed(100)
 lam, mu, Xzero = 2, 1, 1
 T, N = 1, 2**8
-dt = 1/N
+dt = T/N
 t = np.linspace(start=0, stop=T, num=N+1)
 dW = np.random.normal(loc=0, scale=np.sqrt(dt), size=N)
 W = np.insert(arr=np.cumsum(dW), obj=0, values=0)
@@ -25,7 +25,7 @@ R = 1; Dt = R*dt; L = N//R
 Xem = np.zeros(shape=L+1)
 Xtemp = Xzero
 Xem[0] = Xtemp
-for j in range(0, L):
+for j in range(L):
     Winc = np.sum(dW[R*j:R*(j+1)])
     Xtemp += Dt*lam*Xtemp + mu*Xtemp*Winc
     Xem[j+1] = Xtemp
