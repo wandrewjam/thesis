ba8261756957d176acc95ef5c22aa84a50c2ae03
Andrew Watson <awatson@math.utah.edu>
2018-10-30 08:03:58 -0600

    Corrected the sampling function

--- rolling-model/sampling.py
+++ rolling-model/sampling.py
@@ -6,14 +6,14 @@ def sample_velocity(t, v, t_samp=None):
         t_samp = np.linspace(start=0, stop=t[-1], num=100)
         return v[np.searchsorted(t, t_samp)], t_samp
     else:
-        return v[np.searchsorted(t, t_samp)]
+        return v[np.searchsorted(t, t_samp, side='right')-1]
 
 
 def sample_position(t, v, t_samp=None):
     pos = np.cumsum(v[1:]*(t[1:] - t[:-1]))
     if t_samp is None:
         t_samp = np.linspace(start=0, stop=np.max(t), num=100)
-        return np.insert(arr=pos[np.searchsorted(t, t_samp)], obj=0, values=0), t_samp
+        return np.insert(arr=pos[np.searchsorted(t, t_samp, side='right')-1], obj=0, values=0), t_samp
     else:
-        return np.insert(arr=pos[np.searchsorted(t, t_samp)], obj=0, values=0)
+        return np.insert(arr=pos[np.searchsorted(t, t_samp, side='right')-1], obj=0, values=0)
 
