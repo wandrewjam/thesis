44db4c2284ba7be587a0fcd40f17626716dcd1ba
Andrew Watson <awatson@math.utah.edu>
2018-11-08 14:42:41 -0700

    Parallelized code, and changed 'is' testing of strings to '==' testing

--- rolling-model/stochastic_reactions_single.py
+++ rolling-model/stochastic_reactions_single.py
@@ -1,5 +1,6 @@
 # A simple check with binding in one theta position, several z positions
 
+import multiprocessing as mp
 import numpy as np
 import matplotlib.pyplot as plt
 from constructA import length
@@ -19,7 +20,7 @@ def fixed_z(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100
 
     if init is None:
         bond_list = np.empty(shape=(0, 1))
-    elif init is 'sat':
+    elif init == 'sat':
         bond_list = np.random.uniform(low=-L, high=L, size=(bond_max, 1))
     else:
         # Handle a bad init input
@@ -30,27 +31,27 @@ def fixed_z(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100
     t = np.linspace(0, T, time_steps+1)
     dt = t[1]-t[0]
 
-    if ztype is 'cont_exact':
+    if ztype == 'cont_exact':
         # For continuous z, exact rate integral
         expected_coeffs = dt*kap*np.exp(-eta/2*(1 - np.cos(theta) + d_prime)**2)*np.sqrt(np.pi/(2*eta))*(
             erf(np.sqrt(eta/2)*(np.sin(theta) + L)) - erf(np.sqrt(eta/2)*(np.sin(theta) - L))
         )
         a = (-L - np.sin(theta))/np.sqrt(1/eta)
         b = (L - np.sin(theta))/np.sqrt(1/eta)
-    elif ztype is 'cont_approx':
+    elif ztype == 'cont_approx':
         # For continuous z, approximate rate integral
         l_matrix = length(z_vec, theta, d_prime=d_prime)
         expected_coeffs = dt*kap*np.trapz(np.exp(-eta/2*l_matrix**2), z_vec, axis=0)  # I'm not sure about this
         # expected_coeffs = dt*kap*h*np.sum(np.exp(-eta/2*l_matrix**2), axis=0)
         a = (-L - theta)/np.sqrt(1/eta)
         b = (L - theta)/np.sqrt(1/eta)
-    elif ztype is 'discrete':
+    elif ztype == 'discrete':
         # For discrete z
         l_matrix = length(z_vec, theta, d_prime=d_prime)
         expected_coeffs = h*dt*kap*np.exp(-eta/2*l_matrix**2)
 
-    on = (binding is 'both') or (binding is 'on')
-    off = (binding is 'both') or (binding is 'off')
+    on = (binding == 'both') or (binding == 'on')
+    off = (binding == 'both') or (binding == 'off')
     sat = saturation
 
     for k in range(time_steps):
@@ -62,15 +63,15 @@ def fixed_z(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100
 
         # Decide which bonds form
         bond_count = bond_list.shape[0]
-        if ztype is 'cont_exact' or ztype is 'cont_approx':
+        if ztype == 'cont_exact' or ztype == 'cont_approx':
             # Continuous z formulation
             expected_vals = on*expected_coeffs*(bond_max - sat*bond_count)  # Here expected_coeffs is a scalar
-        elif ztype is 'discrete':
+        elif ztype == 'discrete':
             # Discrete z formulation
             expected_vals = on*expected_coeffs*(bond_max - sat*bond_count)  # Here expected_coeffs is a vector
 
         # Generate bonds in each bin
-        if ztype is 'cont_exact' or ztype is 'cont_approx':
+        if ztype == 'cont_exact' or ztype == 'cont_approx':
             # Continuous z formulation
             expected_vals = np.amin(expected_vals, 0)
             forming_bonds = np.random.poisson(lam=expected_vals)
@@ -83,7 +84,7 @@ def fixed_z(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100
                                                                            scale=np.sqrt(1/eta),
                                                                            size=forming_bonds)
             counter += forming_bonds
-        elif ztype is 'discrete':
+        elif ztype == 'discrete':
             # Discrete z formulation
             expected_vals = expected_vals.clip(min=0)
             forming_bonds = np.random.poisson(lam=expected_vals)
@@ -97,9 +98,9 @@ def fixed_z(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100
                 counter += forming_bonds[j, k]
 
         # Update the bond array
-        if binding is 'both' or binding is 'off':
+        if binding == 'both' or binding == 'off':
             bond_list = np.delete(arr=bond_list, obj=break_indices, axis=0)
-        if binding is 'both' or binding is 'on':
+        if binding == 'both' or binding == 'on':
             bond_list = np.append(arr=bond_list, values=new_bonds, axis=0)
 
         # Store the bond list
@@ -117,7 +118,7 @@ def variable_z(theta, init=None, L=2.5, T=0.4, M=100, bond_max=100, d_prime=0.1,
 
     if init is None:
         bond_list = np.empty(shape=(0, 1))
-    elif init is 'sat':
+    elif init == 'sat':
         bond_list = np.random.uniform(low=-L, high=L, size=(bond_max, 1))
     else:
         # Handle a bad init input
@@ -127,27 +128,27 @@ def variable_z(theta, init=None, L=2.5, T=0.4, M=100, bond_max=100, d_prime=0.1,
 
     t = np.array([0])
 
-    if ztype is 'cont_exact':
+    if ztype == 'cont_exact':
         # For continuous z, exact rate integral
         expected_coeffs = kap*np.exp(-eta/2*(1 - np.cos(theta) + d_prime)**2)*np.sqrt(np.pi/(2*eta))*(
             erf(np.sqrt(eta/2)*(np.sin(theta) + L)) - erf(np.sqrt(eta/2)*(np.sin(theta) - L))
         )
         a = (-L - np.sin(theta))/np.sqrt(1/eta)
         b = (L - np.sin(theta))/np.sqrt(1/eta)
-    elif ztype is 'cont_approx':
+    elif ztype == 'cont_approx':
         # For continuous z, approximate rate integral
         l_matrix = length(z_vec, theta, d_prime=d_prime)
         expected_coeffs = kap*np.trapz(np.exp(-eta/2*l_matrix**2), z_vec, axis=0)  # I'm not sure about this
         # expected_coeffs = dt*kap*h*np.sum(np.exp(-eta/2*l_matrix**2), axis=0)
         a = (-L - theta)/np.sqrt(1/eta)
         b = (L - theta)/np.sqrt(1/eta)
-    elif ztype is 'discrete':
+    elif ztype == 'discrete':
         # For discrete z
         l_matrix = length(z_vec, theta, d_prime=d_prime)
         expected_coeffs = h*kap*np.exp(-eta/2*l_matrix**2)
 
-    on = (binding is 'both') or (binding is 'on')
-    off = (binding is 'both') or (binding is 'off')
+    on = (binding == 'both') or (binding == 'on')
+    off = (binding == 'both') or (binding == 'off')
     sat = saturation
 
     while t[-1] < T:
@@ -157,15 +158,15 @@ def variable_z(theta, init=None, L=2.5, T=0.4, M=100, bond_max=100, d_prime=0.1,
 
         # Decide which bonds form
         bond_counts = bond_list.shape[0]
-        if ztype is 'cont_exact' or ztype is 'cont_approx':
+        if ztype == 'cont_exact' or ztype == 'cont_approx':
             # Continuous z formulation
             form_rates = on*expected_coeffs*(bond_max - sat*bond_counts)  # Calculate the expected values
-        elif ztype is 'discrete':
+        elif ztype == 'discrete':
             # Discrete z formulation
             form_rates = on*expected_coeffs*(bond_max - sat*bond_counts)
 
         # Generate bonds in each bin
-        if ztype is 'cont_exact' or ztype is 'cont_approx':
+        if ztype == 'cont_exact' or ztype == 'cont_approx':
             # Continuous z formulation
             all_rates = np.append(break_rates, form_rates)
             sum_rates = np.cumsum(all_rates)
@@ -183,7 +184,7 @@ def variable_z(theta, init=None, L=2.5, T=0.4, M=100, bond_max=100, d_prime=0.1,
                 new_bonds = np.zeros(shape=(1, 1))
                 new_bonds[0, 0] = truncnorm.rvs(a=a, b=b, loc=np.sin(theta), scale=np.sqrt(1/eta))
                 bond_list = np.append(arr=bond_list, values=new_bonds, axis=0)
-        elif ztype is 'discrete':
+        elif ztype == 'discrete':
             # Discrete z formulation
             all_rates = np.append(break_rates, form_rates)
             sum_rates = np.cumsum(all_rates)
@@ -219,7 +220,7 @@ def pde_z(theta, init=None, L=2.5, T=.4, M=100, time_steps=1000, d_prime=0.1, et
     dt = t[1]-t[0]
 
     m_mesh = np.zeros(shape=(2*M+1, time_steps+1))  # Bond densities are initialized to zero
-    if init is 'sat':
+    if init == 'sat':
         m_mesh[:, :, 0] = 1/(2*L)
     elif init is not None:
         # Handle a bad init input
@@ -228,8 +229,8 @@ def pde_z(theta, init=None, L=2.5, T=.4, M=100, time_steps=1000, d_prime=0.1, et
 
     l_matrix = length(z_vec, theta, d_prime)
 
-    on = binding is 'both' or binding is 'on'
-    off = binding is 'both' or binding is 'off'
+    on = binding == 'both' or binding == 'on'
+    off = binding == 'both' or binding == 'off'
     sat = saturation
     for i in range(time_steps):
         m_mesh[:, i+1] = (m_mesh[:, i] + on*dt*kap*np.exp(-eta/2*l_matrix**2) *
@@ -238,6 +239,29 @@ def pde_z(theta, init=None, L=2.5, T=.4, M=100, time_steps=1000, d_prime=0.1, et
                              (1 + off*dt*np.exp(delta*l_matrix))
 
     return z_vec, m_mesh, t
+
+
+def count_fixed(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100, d_prime=0.1, eta=0.1,
+            delta=3.0, kap=1.0, saturation=True, binding='both', ztype='cont_exact', seed=None):
+    start = timer()
+    master_list, t = fixed_z(theta, init, L, T, M, time_steps, bond_max, d_prime,
+                             eta, delta, kap, saturation, binding, ztype, seed)
+    count = [master_list[i].shape[0] for i in range(len(master_list))]
+    end = timer()
+    print('Completed one fixed run. This run took {:g} seconds.'.format(end-start))
+    return np.array(count)
+
+
+def count_variable(theta, init=None, L=2.5, T=0.4, M=100, time_steps=1000, bond_max=100, d_prime=0.1, eta=0.1, delta=3.0, kap=1.0,
+               saturation=True, binding='both', ztype='cont_exact', seed=None):
+    start = timer()
+    master_list, t = variable_z(theta, init, L, T, M, bond_max, d_prime, eta,
+                                delta, kap, saturation, binding, ztype, seed)
+    t_sample = np.linspace(0, T, num=time_steps+1)
+    count = [master_list[i].shape[0] for i in range(len(master_list))]
+    end = timer()
+    print('Completed one variable run. This run took {:g} seconds.'.format(end-start))
+    return np.array(count)[np.searchsorted(t, t_sample, side='right')-1]
 
 
 if __name__ == '__main__':
@@ -261,26 +285,23 @@ if __name__ == '__main__':
     z_vec, m_mesh, tp = pde_z(theta, init=init, T=T, M=M, time_steps=time_steps, delta=delta,
                               saturation=sat, binding=binding)
 
-    fixed_arr = np.zeros(shape=(trials, time_steps+1))
-    var_arr = np.zeros(shape=(trials, time_steps+1))
+    pool = mp.Pool(processes=4)
+    fixed_result = [pool.apply_async(count_fixed, args=(theta, ), kwds={'init': init, 'T': T, 'M': M,
+                                                                        'time_steps': time_steps, 'bond_max': bond_max,
+                                                                        'delta': delta, 'saturation': sat,
+                                                                        'binding': binding, 'ztype': ztype})
+                    for _ in range(trials)]
+    var_result = [pool.apply_async(count_variable, args=(theta, ), kwds={'init': init, 'T': T, 'M': M,
+                                                                         'time_steps': time_steps, 'bond_max': bond_max,
+                                                                         'delta': delta, 'saturation': sat,
+                                                                         'binding': binding, 'ztype': ztype})
+                  for _ in range(trials)]
 
-    for k in range(trials):
-        start = timer()
-        temp_list = fixed_z(theta, init=init, T=T, M=M, time_steps=time_steps, bond_max=bond_max, delta=delta,
-                            saturation=sat, binding=binding, ztype=ztype)[0]
-        for j in range(len(temp_list)):
-            fixed_arr[k, j] = temp_list[j].shape[0]
+    fixed_result = [f.get() for f in fixed_result]
+    var_result = [v.get() for v in var_result]
 
-        temp_list, var_t = variable_z(theta, init=init, T=T, M=M, bond_max=bond_max, delta=delta,
-                                      saturation=sat, binding=binding, ztype=ztype)
-        temp_count = np.zeros(shape=len(temp_list))
-        for j in range(len(temp_list)):
-            temp_count[j] = temp_list[j].shape[0]
-        var_arr[k, :] = temp_count[np.searchsorted(var_t, tp, side='right')-1]
-        end = timer()
-        print('{:d} of {:d} trials completed. This run took {:g} seconds.'.format(k+1, trials, end-start))
-        # plt.plot(t, fixed_arr[k, :]/bond_max, t, pde, var_t, np.arange(var_t.shape[0])/bond_max)
-        # plt.show()
+    fixed_arr = np.vstack(fixed_result)
+    var_arr = np.vstack(var_result)
 
     fixed_avg = np.mean(fixed_arr, axis=0)
     var_avg = np.mean(var_arr, axis=0)
@@ -291,7 +312,7 @@ if __name__ == '__main__':
 
     # Define parameter array and filename, and save the count data
     # The time is included to prevent overwriting an existing file
-    par_array = np.array([delta, T, init, sat, binding, M, bond_max, L, ztype])
+    par_array = np.array([delta, T, init, sat, binding, M, bond_max, trials, L, ztype])
     file_path = './data/sta_rxns/'
     file_name = 'singlerec_M{0:d}_theta{1:g}_trials{2:d}_{3:s}.npz'.format(M, theta, trials, strftime('%d%m%y'))
     np.savez_compressed(file_path+file_name, par_array, fixed_arr, var_arr, pde_count, tp, par_array=par_array,
@@ -314,8 +335,8 @@ if __name__ == '__main__':
         plt.plot(tp, var_avg/bond_max, 'g', label='Variable time step')
         plt.plot(tp, (fixed_avg + 2*fixed_std/np.sqrt(trials))/bond_max, 'b:',
                  tp, (fixed_avg - 2*fixed_std/np.sqrt(trials))/bond_max, 'b:', linewidth=.5)
-        plt.plot(tp, (var_avg + 2*var_std/np.sqrt(trials))/bond_max, 'b:',
-                 tp, (var_avg - 2*var_std/np.sqrt(trials))/bond_max, 'b:', linewidth=.5)
+        plt.plot(tp, (var_avg + 2*var_std/np.sqrt(trials))/bond_max, 'g:',
+                 tp, (var_avg - 2*var_std/np.sqrt(trials))/bond_max, 'g:', linewidth=.5)
         plt.plot(tp, pde_count, 'k', label='Deterministic')
         plt.legend(loc='best')
         plt.title('Bond numbers for each algorithm for a single receptor')
