96f474495ed58a6b83e288ac136b50f99319604b
Andrew Watson <awatson@math.utah.edu>
2018-11-29 12:35:44 -0700

    Added 3 default numerical parameters

--- rolling-model/default_values.py
+++ rolling-model/default_values.py
@@ -1,10 +1,11 @@
-# The default biological/physical values for the rolling model
+# The default parameter values for the rolling model
 
 """
 All functions for the rolling model will use parameter values defined in
 this script, unless overwritten by **kwargs.
 """
 
+# Biological/Physical parameters
 gamma = 20
 kappa = 1
 eta = .1
@@ -17,3 +18,8 @@ xi_v = .001
 xi_om = .001
 v_f = (1 + d)*gamma
 om_f = gamma
+
+# Numerical parameters
+L = 2.5
+T = 1
+bond_max = 10
