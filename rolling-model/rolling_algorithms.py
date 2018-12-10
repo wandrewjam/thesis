05eee14fd0ebf4042ae29f3e05f5c7e90d809301
Andrew Watson <awatson@math.utah.edu>
2018-12-10 15:15:47 -0700

    More compartmentalizing of the new eulerian code. Still not ready.

--- rolling-model/rolling_algorithms.py
+++ rolling-model/rolling_algorithms.py
@@ -21,47 +21,101 @@ from default_values import set_parameters
 from constructA import nd_force, nd_torque, length
 
 
-def pde_eulerian(M, N, time_steps, m0, **kwargs):
-    """This is a function to solve the full eulerian PDE model."""
-
-    # Define the problem parameters.
-    (v_f, om_f, kappa, eta, d, delta, on, off, sat, xi_v, xi_om, L, T,
-     save_bond_history) = set_parameters(**kwargs)
+def _handle_velocities(v_f, om_f):
+    """
+    This function converts a scalar v_f and om_f to an array of
+    floats, if necessary
+    """
 
     if type(v_f) is float or int:
-        v_f = v_f*np.ones(shape=time_steps+1)
+        v_f = (v_f*np.ones(shape=time_steps+1)).astype(dtype=float)
     if type(om_f) is float or int:
-        om_f = om_f*np.ones(shape=time_steps+1)
+        om_f = (om_f*np.ones(shape=time_steps+1)).astype(dtype=float)
 
-    # Define coordinate meshes and mesh widths.
+    return v_f, om_f
+
+
+def _cfl_check(v_f, om_f, dt, h, nu, scheme):
+    """ This function checks the CFL condition """
+
+    if scheme == 'up':
+        z_check, om_check = np.max(v_f)*dt/h, np.max(om_f)*dt/nu
+    elif scheme == 'bw':
+        z_check, om_check = np.max(v_f)*dt/(2*h), np.max(om_f)*dt/(2*nu)
+    else:
+        raise Exception('the parameter \'scheme\' is not valid')
+
+    if z_check > 1 and om_check > 1:
+        raise ValueError('the CFL conditions for both theta and z are not '
+                         'satisfied')
+    elif z_check > 1:
+        raise ValueError('the CFL condition for z is not satisfied')
+    elif om_check > 1:
+        raise ValueError('the CFL condition for theta is not satisfied')
+
+
+def _generate_coordinate_arrays(M, N, time_steps, L, T, d):
     z_mesh, th_mesh = np.meshgrid(np.linspace(-L, L, 2*M+1),
                                   np.linspace(-np.pi/2, np.pi/2, N+1),
                                   indexing='ij')
+    l_mesh = length(z_mesh, th_mesh, d)
     t = np.linspace(0, T, time_steps+1)
     h = z_mesh[1, 0] - z_mesh[0, 0]
     nu = th_mesh[0, 1] - th_mesh[0, 0]
     dt = t[1]-t[0]
 
-    # Define distance/length mesh and formation/breaking rate meshes.
-    l_matrix = length(z_mesh, th_mesh, d)
-    form_rate = on*kappa*np.exp(-eta/2*l_matrix[:-1, :-1]**2)
-    break_rate = off*np.exp(delta*l_matrix[:-1, :-1])
+    return z_mesh, th_mesh, l_mesh, t, h, nu, dt
+
+
+def _initialize_unknowns(v_f, om_f, m0, time_steps, save_bond_history):
+    v, om = np.zeros(shape=v_f.shape), np.zeros(shape=om_f)
+    v[0], om[0] = v_f[0], om_f[0]
+
+    if save_bond_history:
+        bond_mesh = np.zeros(shape=m0.shape+(time_steps,))
+        bond_mesh[:, :, 0] = m0
+    else:
+        bond_mesh = np.copy(m0)
+
+    return v, om, bond_mesh
+
+
+def _upwind(v, om, bond_mesh, v_f, om_f, form_rate, break_rate,
+            save_bond_history):
+    """ This function carries out the upwind scheme """
+
+    time_steps = v.shape[0]
+
+    if save_bond_history:
+        for i in range(time_steps + 1):
+            bond_mesh[:, :, i+1] = _upwind_step()
+
+    return v, om, bond_mesh, forces, torques
+
+
+def pde_eulerian(M, N, time_steps, m0, **kwargs):
+    """This is a function to solve the full eulerian PDE model."""
+
+    # Define the problem parameters.
+    (v_f, om_f, kappa, eta, d, delta, on, off, sat, xi_v, xi_om, L, T,
+     save_bond_history, scheme) = set_parameters(**kwargs)
+
+    v_f, om_f = _handle_velocities(v_f, om_f)
+
+    # Define coordinate meshes and mesh widths.
+    (z_mesh, th_mesh, l_mesh, t, h, nu, dt) = (
+        _generate_coordinate_arrays(M, N, time_steps, L, T, d)
+    )
 
     # Check for the CFL condition
-    if np.max(om_f)*dt/nu > 1 and np.max(v_f)*dt/h > 1:
-        raise ValueError('the CFL conditions for theta and z are not '
-                         'satisfied')
-    elif np.max(v_f)*dt/h > 1:
-        raise ValueError('the CFL condition for z is not satisfied!')
-    elif np.max(om_f)*dt/nu > 1:
-        raise ValueError('the CFL condition for theta is not satisfied!')
+    _cfl_check(v_f, om_f, dt, h, nu)
 
-    # Initialize velocity vectors
-    v = np.zeros(shape=time_steps+1)
-    v[0] = v_f[0]
+    v, om, bond_mesh = _initialize_unknowns(v_f, om_f, m0, time_steps,
+                                            save_bond_history)
 
-    om = np.zeros(shape=time_steps+1)
-    om[0] = om_f[0]
+    # Define distance/length mesh and formation/breaking rate meshes.
+    form_rate = on*kappa*np.exp(-eta/2*l_mesh[:-1, :-1]**2)
+    break_rate = off*np.exp(delta*l_mesh[:-1, :-1])
 
     def time_step(v_f, om_f, v, om, bond_mesh):
         """
@@ -96,25 +150,33 @@ def pde_eulerian(M, N, time_steps, m0, **kwargs):
 
         return bond_mesh, new_v, new_om
 
-    if save_bond_history:
-        m_mesh = np.zeros(shape=(2*M+1, N+1, time_steps+1))
-        m_mesh[:, :, 0] = m0
+    # if save_bond_history:
+    #     m_mesh = np.zeros(shape=(2*M+1, N+1, time_steps+1))
+    #     m_mesh[:, :, 0] = m0
+    #
+    #     for i in range(time_steps):
+    #         m_mesh[:, :, i+1], v[i+1], om[i+1] = (
+    #             time_step(v_f[i], om_f[i], v[i], om[i],
+    #                       bond_mesh=m_mesh[:, :, i])
+    #         )
+    #
+    # else:
+    #     m_mesh = m0
+    #
+    #     for i in range(time_steps):
+    #         m_mesh, v[i+1], om[i+1] = (
+    #             time_step(v_f[i], om_f[i], v[i], om[i], bond_mesh=m_mesh)
+    #         )
 
-        for i in range(time_steps):
-            m_mesh[:, :, i+1], v[i+1], om[i+1] = (
-                time_step(v_f[i], om_f[i], v[i], om[i],
-                          bond_mesh=m_mesh[:, :, i])
-            )
+    if scheme == 'up':
+        v, om, bond_mesh, forces, torques = (
+            _upwind(v, om, bond_mesh, v_f, om_f, form_rate, break_rate)
+        )
+    elif scheme == 'bw':
+        v, om, bond_mesh, forces, torques = _beam_warming(v, om, bond_mesh,
+                                                          form_rate, break_rate)
 
-    else:
-        m_mesh = m0
-
-        for i in range(time_steps):
-            m_mesh, v[i+1], om[i+1] = (
-                time_step(v_f[i], om_f[i], v[i], om[i], bond_mesh=m_mesh)
-            )
-
-    return v, om, m_mesh, t
+    return v, om, bond_mesh, t
 
 
 if __name__ == '__main__':
