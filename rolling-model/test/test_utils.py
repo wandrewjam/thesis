7434e8a6e78006eac4064a12f90904ab30583e83
Andrew Watson <awatson@math.utah.edu>
2019-02-03 11:34:45 -0700

    Add test to test_utils

--- rolling-model/test/test_utils.py
+++ rolling-model/test/test_utils.py
@@ -4,9 +4,9 @@ from utils import length
 
 
 class TestLength(TestCase):
-    scalar_z, scalar_th = 0, 0
-    array_z = np.linspace(-1, 1, num=3)
-    array_th = np.linspace(-np.pi/2, np.pi/2, num=3)
+    def setUp(self):
+        self.arrayz = np.linspace(-1, 1, num=3)
+        self.arrayth = np.linspace(-np.pi/2, np.pi/2, num=3)
 
     def test_zero_scalars(self):
         self.assertEqual(length(z=0, th=0, d=0), 0)
@@ -15,16 +15,22 @@ class TestLength(TestCase):
         self.assertEqual(length(z=0, th=0, d=.1), .1)
 
     def test_scalarz_arrayth(self):
-        array_th = np.linspace(-np.pi/2, np.pi/2, num=3)
         self.assertLessEqual(
-            np.amax(np.abs(length(z=0, th=array_th, d=0)
+            np.amax(np.abs(length(z=0, th=self.arrayth, d=0)
                            - np.array([np.sqrt(2), 0, np.sqrt(2)]))),
             2*np.finfo(float).eps
         )
 
     def test_arrayz_scalarth(self):
-        arrayz = np.linspace(-1, 1, num=3)
         self.assertEqual(
-            np.amax(np.abs(length(z=arrayz, th=0, d=0)
+            np.amax(np.abs(length(z=self.arrayz, th=0, d=0)
                            - np.array([1, 0, 1]))), 0
         )
+
+    def test_arrayz_arrayth(self):
+        arrayth = np.linspace(-np.pi/2, np.pi/2, num=5)
+        self.assertRaises(ValueError, length, self.arrayz, arrayth, 0)
+
+class TestTorque(TestCase):
+    def test_torque(self):
+        self.assertEqual(0, 0)
