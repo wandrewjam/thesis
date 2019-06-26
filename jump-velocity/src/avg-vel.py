4b706687bfd7736d4adaf1acd086f19c3c6af500
Andrew Watson <awatson@math.utah.edu>
2019-06-26 17:11:45 -0600

    Modify velocity plot appearance

--- jump-velocity/src/avg-vel.py
+++ jump-velocity/src/avg-vel.py
@@ -8,13 +8,15 @@ def main(filename, show_plot=False):
     plot_dir = 'plots/'
     data = np.load(npz_dir + filename + '.npz')
     y, s_eval = data['y'], data['s_eval']
-    u1_bdy = data['u1_bdy']
+    u0_bdy, u1_bdy = data['u0_bdy'], data['u1_bdy']
 
-    s_mask = s_eval > 2. / 3
-    F = cumtrapz(u1_bdy, s_eval, initial=0)
+    s_mask = s_eval > 4./5
+    F = cumtrapz(u0_bdy + u1_bdy, s_eval, initial=0)
     fig_av, ax_av = plt.subplots()
-    ax_av.plot(1 / s_eval[s_mask], s_eval[s_mask] ** 2 * u1_bdy[s_mask], 1 / s_eval[s_mask], 1 - F[s_mask])
+    ax_av.plot(1 / s_eval[s_mask], s_eval[s_mask] ** 2 * u1_bdy[s_mask], label='Velocity PDF of filtered plts')
+    ax_av.plot(1 / s_eval[s_mask], 1 - F[s_mask], label='Velocity CDF of all plts')
     ax_av.axvline(1, color='k')
+    ax_av.legend(loc='upper right')
     ax_av.set_xlabel('$v^*$')
     ax_av.set_ylabel('Probability density')
     fig_av.savefig(plot_dir + filename + '.png')
