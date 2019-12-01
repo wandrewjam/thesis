ada1b082bf449d1ac1092134c1ac8e269260aaf8
Andrew Watson <awatson@math.utah.edu>
2019-12-01 10:46:43 -0700

    Fit distributions to small steps

--- jump-velocity/src/filter_steps.py
+++ jump-velocity/src/filter_steps.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import expon, gamma, norm, uniform, truncnorm
+from scipy.special import gammaln
 import os
 import sys
 
@@ -9,10 +10,13 @@ if __name__ == '__main__':
     os.chdir(os.path.expanduser('~/thesis/jump-velocity'))
 
     sim_dir = 'dat-files/simulations/'
-    data = np.loadtxt(sim_dir + filename + '-step.dat')
+    # data = np.loadtxt(sim_dir + filename + '-step.dat')
+    #
+    # new_data = data[data < 2.]
+    # np.savetxt(sim_dir + filename + '-small-step.dat', new_data)
+    new_data = np.loadtxt(sim_dir + filename + '-small-step.dat')
 
-    new_data = data[data < 2.]
-    np.savetxt(sim_dir + filename + '-small-step.dat', new_data)
+    n = len(new_data)
 
     dst_dir = 'dat-files/distributions/'
     xplot = np.linspace(0, 2, num=201)
@@ -22,29 +26,43 @@ if __name__ == '__main__':
 
     # Put in AIC estimation stuff?
 
-    print('Exponential: lam={}'.format(scale))
+    e_ll = -n * np.log(scale) - n * scale**(-2)
+    e_aic = 2 - 2 * e_ll
+
+    print('Exponential: lam={}. AIC: {}'.format(scale, e_aic))
 
     e_pdf = e_rv.pdf(xplot)
     e_cdf = e_rv.cdf(xplot)
 
-    loc, scale = uniform.fit(new_data)
+    loc, scale = 0, 2
     u_rv = uniform(loc=loc, scale=scale)
 
-    print('Uniform: a={}, b={}'.format(loc, loc+scale))
+    u_ll = -n * np.log(scale)
+    u_aic = -2 * u_ll
+
+    print('Uniform: a={}, b={}. AIC: {}'.format(loc, loc+scale, u_aic))
 
     u_pdf = u_rv.pdf(xplot)
     u_cdf = u_rv.cdf(xplot)
 
     mu, std = np.mean(new_data), np.std(new_data)
+    n_aic = (4 + n * np.log(2 * np.pi) + n * np.log(std**2)
+             + np.sum((new_data - mu)**2) / std**2)
 
-    print('Normal: mu={}, std={}'.format(mu, std))
+    print('Normal: mu={}, std={}. AIC: {}'.format(mu, std, n_aic))
 
     n_rv = norm(loc=mu, scale=std)
     n_pdf = n_rv.pdf(xplot)
     n_cdf = n_rv.cdf(xplot)
 
-    g_rv = gamma(a=mu**2/std**2, scale=std**2/mu)
-    print('Gamma: theta={}, k={}'.format(std**2/mu, mu**2/std**2))
+    a, scale = gamma.fit(new_data, floc=0)[::2]
+    g_rv = gamma(a=a, scale=scale)
+
+    g_ll = ((a - 1) * np.sum(np.log(new_data)) - n * mu / scale
+            - n * a * np.log(scale) - n * gammaln(a))
+    g_aic = 4 - 2 * g_ll
+
+    print('Gamma: theta={}, k={}. AIC: {}'.format(scale, a, g_aic))
 
     g_pdf = g_rv.pdf(xplot)
     g_cdf = g_rv.cdf(xplot)
