3f25b21d4ec61e612e544592a3f92f02749fe84b
Andrew Watson <awatson@math.utah.edu>
2020-02-25 12:24:03 -0700

    Test body force computed from velocity

--- reg_stokeslets/3d/force_test.py
+++ reg_stokeslets/3d/force_test.py
@@ -21,14 +21,23 @@ def find_column(n_nodes, center, force_center, k, eps):
 def main(proc=1):
     assert proc > 0, 'proc must be a positive integer'
     assert type(proc) is int, 'proc must be a positive integer'
-    n_nodes = 8
-    eps = 0.01
-    sphere_nodes = generate_grid(n_nodes)[-1]
+    eps = 0.1
+    top = 2
+
+    l2_error = np.zeros(shape=(top, 2))
+    for i in range(top):
+        n_nodes = 2**(i + 1)
+        l2_error[i, 0] = find_solve_error(eps, n_nodes, proc)
+        l2_error[i, 1] = n_nodes
+
+        np.savetxt('find_solve_error.txt', l2_error)
+
+
+def find_solve_error(eps, n_nodes, proc):
+    sphere_nodes = generate_grid(n_nodes)[2]
     total_nodes = sphere_nodes.shape[0]
     a_matrix = np.zeros(shape=(3 * total_nodes, 3 * total_nodes))
-
     pool = mp.Pool(processes=proc)
-
     for i in range(total_nodes):
         for k in range(3):
             if proc == 1:
@@ -45,24 +54,16 @@ def main(proc=1):
                     }) for point in sphere_nodes]
                 column = [res.get() for res in col_result]
             a_matrix[:, 3 * i + k] = np.concatenate(column)
-    const_force = np.tile([0, 0, -3. / 2], total_nodes)
-    result = np.dot(a_matrix, const_force)
-
-    def cforce_fn(x):
-        out_array = np.zeros(shape=x.shape)
-        out_array[..., 2] = -3. / 2
-        return out_array
-
-    direct_integral = [
-        sphere_integrate(stokeslet_integrand, n_nodes=n_nodes,
-                         center=point, force=cforce_fn, eps=eps)
-        for point in sphere_nodes
-    ]
-    print(np.amax(result - np.concatenate(direct_integral)))
     unit_vel = np.tile([0, 0, -1], total_nodes)
-    print(np.linalg.solve(a_matrix, unit_vel))
+    est_force = np.linalg.lstsq(a_matrix, unit_vel)[0]
+    est_force = est_force.reshape((-1, 3))
+
+    total_force = sphere_integrate(est_force, n_nodes=n_nodes)
+    error = np.abs(total_force[2] - 6 * np.pi)
+
+    return error
 
 
 if __name__ == '__main__':
-    cProfile.run('main()')
-    # main(3)
+    # cProfile.run('main()')
+    main(3)
