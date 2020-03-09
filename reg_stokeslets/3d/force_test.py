b983212a421f158c003f9676c77f9d307fa35689
Andrew Watson <awatson@math.utah.edu>
2020-03-08 21:21:48 -0600

    Finish generalizing code to handle an ellipsoid

--- reg_stokeslets/3d/force_test.py
+++ reg_stokeslets/3d/force_test.py
@@ -48,20 +48,28 @@ def find_solve_error(eps, n_nodes):
     return error
 
 
-def assemble_quad_matrix(eps, n_nodes):
-    xi_mesh, eta_mesh, sphere_nodes, ind_map = generate_grid(n_nodes)
+def assemble_quad_matrix(eps, n_nodes, a=1., b=1.):
+    xi_mesh, eta_mesh, nodes, ind_map = generate_grid(n_nodes, a=a, b=b)
 
     c_matrix = np.ones(shape=ind_map.shape[:2])
     c_matrix[(0, n_nodes), 1:n_nodes] = 1. / 2
     c_matrix[1:n_nodes, (0, n_nodes)] = 1. / 2
     c_matrix[0:n_nodes + 1:n_nodes, 0:n_nodes + 1:n_nodes] = 1. / 3
-    weight_array = c_matrix * geom_weights(xi_mesh[:, np.newaxis], eta_mesh[np.newaxis, :])
-    weight_array = np.tile(weight_array[:, :, np.newaxis], (1, 1, 6))
+
+    # Need to modify this for the ellipse
+    weight_array = [
+        c_matrix * geom_weights(xi_mesh[:, np.newaxis],
+                                eta_mesh[np.newaxis, :], patch=patch)
+        for patch in range(1, 7)
+    ]
+    # weight_array = c_matrix * geom_weights(xi_mesh[:, np.newaxis], eta_mesh[np.newaxis, :])
+    # weight_array = np.tile(weight_array[:, :, np.newaxis], (1, 1, 6))
+    weight_array = np.stack(weight_array, axis=-1)
     weights = np.array([sum(weight_array[ind_map == i])
-                        for i in range(sphere_nodes.shape[0])])
+                        for i in range(nodes.shape[0])])
     del_xi = xi_mesh[1] - xi_mesh[0]
     del_eta = eta_mesh[1] - eta_mesh[0]
-    del_x = sphere_nodes[:, np.newaxis, :] - sphere_nodes[np.newaxis, :, :]
+    del_x = nodes[:, np.newaxis, :] - nodes[np.newaxis, :, :]
     r2 = np.sum(del_x ** 2, axis=-1)
     assert sum(np.diag(r2)) == 0
     assert np.all(r2 == r2.T)
@@ -72,8 +80,8 @@ def assemble_quad_matrix(eps, n_nodes):
     assert np.all(stokeslet == stokeslet.transpose((1, 0, 3, 2)))
     a_matrix = -stokeslet * weights[:, np.newaxis, np.newaxis] / (8 * np.pi)
     a_matrix = a_matrix.transpose((0, 2, 1, 3)).reshape(
-        (sphere_nodes.size, sphere_nodes.size)) * del_xi * del_eta
-    return a_matrix, sphere_nodes
+        (nodes.size, nodes.size)) * del_xi * del_eta
+    return a_matrix, nodes
 
 
 if __name__ == '__main__':
--- reg_stokeslets/3d/resistance_matrix_test.py
+++ reg_stokeslets/3d/resistance_matrix_test.py
@@ -18,48 +18,53 @@ def assemble_vel_cases(sphere_nodes):
     return np.hstack([v_array, om_array, shear_vec[:, np.newaxis]])
 
 
-def main(proc=1):
-    eps = [0.1, 0.05, 0.01]
-    n_nodes = [12, 24, 36, 48]
+def main(proc=1, a=1., b=1.):
+    eps = [0.1, 0.05]
+    n_nodes = [4, 8]
 
     if proc == 1:
-        matrices = [generate_resistance_matrices(e, n)
+        matrices = [generate_resistance_matrices(e, n, a, b)
                     for e in eps for n in n_nodes]
     else:
         pool = mp.Pool(processes=proc)
-        result = [pool.apply_async(generate_resistance_matrices, args=(e, n))
+        result = [pool.apply_async(generate_resistance_matrices, args=(e, n),
+                                   kwds={'a': a, 'b': b})
                   for e in eps for n in n_nodes]
         matrices = [res.get() for res in result]
 
-    T = np.zeros(shape=(len(eps), len(n_nodes) + 1))
-    R = np.zeros(shape=(len(eps), len(n_nodes) + 1))
-    S = np.zeros(shape=(len(eps), len(n_nodes) + 1))
+    T = np.zeros(shape=(len(eps) * 3, len(n_nodes) * 3 + 1))
+    R = np.zeros(shape=(len(eps) * 3, len(n_nodes) * 3 + 1))
+    S = np.zeros(shape=(len(eps) * 3, len(n_nodes) * 2 + 1))
 
-    T[:, 0] = eps
-    R[:, 0] = eps
-    S[:, 0] = eps
+    T[::3, 0] = eps
+    R[::3, 0] = eps
+    S[::3, 0] = eps
 
     for i in range(len(eps)):
         for j in range(len(n_nodes)):
-            T[i, j+1] = matrices[len(n_nodes) * i + j][0][0, 0]
-            R[i, j+1] = matrices[len(n_nodes) * i + j][3][0, 0]
-            S[i, j+1] = matrices[len(n_nodes) * i + j][5][0]
+            T[3*i:3*(i+1), 3*j+1:3*(j+1)+1] = matrices[len(n_nodes) * i + j][0]
+            R[3*i:3*(i+1), 3*j+1:3*(j+1)+1] = matrices[len(n_nodes) * i + j][3]
+            S[3*i:3*(i+1), 2*j+1] = matrices[len(n_nodes) * i + j][4]
+            S[3*i:3*(i+1), 2*j+2] = matrices[len(n_nodes) * i + j][5]
 
     header = 'eps'
     for n in n_nodes:
         header += ' {}'.format(n)
-    np.savetxt('t_matrix_data.dat', T, header=header)
-    np.savetxt('r_matrix_data.dat', R, header=header)
-    np.savetxt('s_vector_data.dat', S, header=header)
+
+    # Need to save more info on the matrices
+    np.savetxt('t_matrix_data_{}_{}.dat'.format(a, b), T, header=header)
+    np.savetxt('r_matrix_data_{}_{}.dat'.format(a, b), R, header=header)
+    np.savetxt('s_vector_data_{}_{}.dat'.format(a, b), S, header=header)
 
 
-def generate_resistance_matrices(eps, n_nodes):
+def generate_resistance_matrices(eps, n_nodes, a=1., b=1.):
     print('Assembling quadrature matrix for eps = {}, nodes = {}'.format(
         eps, n_nodes))
-    a_matrix, sphere_nodes = assemble_quad_matrix(eps=eps, n_nodes=n_nodes)
+    a_matrix, nodes = assemble_quad_matrix(eps=eps, n_nodes=n_nodes,
+                                                  a=a, b=b)
     # Solve for the forces given 6 different velocity cases
     print('Assembling rhs for eps = {}, nodes = {}'.format(eps, n_nodes))
-    rhs = assemble_vel_cases(sphere_nodes)
+    rhs = assemble_vel_cases(nodes)
     print('Solving for forces for eps = {}, nodes = {}'.format(eps, n_nodes))
     try:
         pt_forces = -np.linalg.solve(a_matrix, rhs)
@@ -69,14 +74,16 @@ def generate_resistance_matrices(eps, n_nodes):
     # For each velocity case, integrate velocities to get body force and
     #   body torque
     pt_forces = pt_forces.reshape((-1, 3, 7))
-    pt_torques = np.cross(sphere_nodes[:, :, np.newaxis], pt_forces, axis=1)
+    pt_torques = np.cross(nodes[:, :, np.newaxis], pt_forces, axis=1)
     print('Calculating body force and torque for eps = {}, nodes = {}'.format(
         eps, n_nodes))
     tmp_matrix1 = np.stack([
-        sphere_integrate(pt_forces[..., i], n_nodes=n_nodes) for i in range(7)
+        sphere_integrate(pt_forces[..., i], n_nodes=n_nodes, a=a, b=b)
+        for i in range(7)
     ], axis=-1)
     tmp_matrix2 = np.stack([
-        sphere_integrate(pt_torques[..., i], n_nodes=n_nodes) for i in range(7)
+        sphere_integrate(pt_torques[..., i], n_nodes=n_nodes, a=a, b=b)
+        for i in range(7)
     ], axis=-1)
 
     print('Constructing resistance matrices for eps = {}, nodes = {}'.format(
@@ -90,4 +97,4 @@ def generate_resistance_matrices(eps, n_nodes):
 
 if __name__ == '__main__':
     # cProfile.run('main()')
-    main()
+    main(a=1.5, b=0.5)
