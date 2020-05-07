57038e4bd55bd668df5fd43179092478ece7fa64
Andrew Watson <awatson@math.utah.edu>
2020-05-07 10:48:34 -0600

    Test shear vector on a sphere and correct wall bounded torque calculations

--- reg_stokeslets/3d/resistance_matrix_test.py
+++ reg_stokeslets/3d/resistance_matrix_test.py
@@ -7,24 +7,29 @@ from sphere_integration_utils import sphere_integrate
 import multiprocessing as mp
 
 
-def assemble_vel_cases(sphere_nodes):
+def assemble_vel_cases(sphere_nodes, shear_rate=1., shear_vec=True):
     v_array = np.tile(np.eye(3), (sphere_nodes.shape[0], 1))
     om_array = np.cross(np.eye(3)[np.newaxis, :, :],
                         sphere_nodes[:, np.newaxis, :],
                         axisc=1).reshape((-1, 3))
-    strain = np.zeros(shape=(3, 3, 3))
-    strain[(1, 2), (2, 1), 0] = 1
-    strain[(0, 2), (2, 0), 1] = 1
-    strain[(0, 1), (1, 0), 2] = 1
 
-    e_array = np.dot(strain, sphere_nodes.T).transpose((2, 0, 1))
-    e_array = e_array.reshape((-1, 3))
+    if shear_vec:
+        shear_vec = np.zeros(shape=(3*sphere_nodes.shape[0], 1))
+        shear_vec[2::3, 0] = -sphere_nodes[:, 0] * shear_rate
+        return np.hstack([v_array, om_array, shear_vec])
+    else:
+        strain = np.zeros(shape=(3, 3, 3))
+        strain[(1, 2), (2, 1), 0] = 1
+        strain[(0, 2), (2, 0), 1] = 1
+        strain[(0, 1), (1, 0), 2] = 1
 
-    return np.hstack([v_array, om_array, e_array])
+        e_array = np.dot(strain, sphere_nodes.T).transpose((2, 0, 1))
+        e_array = e_array.reshape((-1, 3))
+        return np.hstack([v_array, om_array, e_array])
 
 
-def generate_resistance_matrices(eps, n_nodes, a=1., b=1., domain='free',
-                                 distance=0, theta=0., phi=0.):
+def generate_resistance_matrices(eps, n_nodes, a=1., b=1., domain='free', distance=0., theta=0., phi=0.,
+                                 shear_vec=True):
     print('Assembling quadrature matrix for eps = {}, nodes = {}'.format(
         eps, n_nodes))
     a_matrix, nodes = assemble_quad_matrix(eps=eps, n_nodes=n_nodes, a=a, b=b,
@@ -32,7 +37,8 @@ def generate_resistance_matrices(eps, n_nodes, a=1., b=1., domain='free',
                                            distance=distance)
     # Solve for the forces given 6 different velocity cases
     print('Assembling rhs for eps = {}, nodes = {}'.format(eps, n_nodes))
-    rhs = assemble_vel_cases(nodes)
+    rhs = assemble_vel_cases(nodes, shear_vec=shear_vec)
+    rhs_cases = rhs.shape[1]
     print('Solving for forces for eps = {}, nodes = {}'.format(eps, n_nodes))
     try:
         pt_forces = -np.linalg.solve(a_matrix, rhs)
@@ -41,27 +47,29 @@ def generate_resistance_matrices(eps, n_nodes, a=1., b=1., domain='free',
         pt_forces = -np.linalg.lstsq(a_matrix, rhs)[0]
     # For each velocity case, integrate velocities to get body force and
     #   body torque
-    pt_forces = pt_forces.reshape((-1, 3, 9))
-    pt_torques = np.cross(nodes[:, :, np.newaxis], pt_forces, axis=1)
+    pt_forces = pt_forces.reshape((-1, 3, rhs_cases))
+    # Need to use the displacement from the center of mass for torques
+    centered_nodes = nodes - distance * np.array([1, 0, 0])
+    pt_torques = np.cross(centered_nodes[:, :, np.newaxis], pt_forces, axis=1)
     print('Calculating body force and torque for eps = {}, nodes = {}'.format(
         eps, n_nodes))
     tmp_matrix1 = np.stack([
         sphere_integrate(pt_forces[..., i], n_nodes=n_nodes, a=a, b=b)
-        for i in range(9)
+        for i in range(rhs_cases)
     ], axis=-1)
     tmp_matrix2 = np.stack([
         sphere_integrate(pt_torques[..., i], n_nodes=n_nodes, a=a, b=b)
-        for i in range(9)
+        for i in range(rhs_cases)
     ], axis=-1)
 
     print('Constructing resistance matrices for eps = {}, nodes = {}'.format(
         eps, n_nodes))
-    t_matrix, p_matrix, e1_matrix = (tmp_matrix1[:, :3], tmp_matrix1[:, 3:6],
-                                     tmp_matrix1[:, 6:])
-    pt_matrix, r_matrix, e2_matrix = (tmp_matrix2[:, :3], tmp_matrix2[:, 3:6],
-                                      tmp_matrix2[:, 6:])
+    t_matrix, p_matrix, shear_f = (tmp_matrix1[:, :3], tmp_matrix1[:, 3:6],
+                                   tmp_matrix1[:, 6:])
+    pt_matrix, r_matrix, shear_t = (tmp_matrix2[:, :3], tmp_matrix2[:, 3:6],
+                                    tmp_matrix2[:, 6:])
     print('Finished eps={}, n_nodes={}'.format(eps, n_nodes))
-    return t_matrix, p_matrix, pt_matrix, r_matrix, e1_matrix, e2_matrix
+    return t_matrix, p_matrix, pt_matrix, r_matrix, shear_f, shear_t
 
 
 def main(proc=1, a=1., b=1., domain='free', distance=0, server='mac'):
@@ -75,16 +83,20 @@ def main(proc=1, a=1., b=1., domain='free', distance=0, server='mac'):
         raise ValueError('\'server\' variable is not valid')
 
     if proc == 1:
-        matrices = [generate_resistance_matrices(e, n, a, b, domain=domain,
-                                                 distance=distance)
-                    for e in eps for n in n_nodes]
+        matrices = [
+            generate_resistance_matrices(e, n, a, b, domain=domain,
+                                         distance=distance, shear_vec=False)
+            for e in eps for n in n_nodes
+        ]
     else:
         pool = mp.Pool(processes=proc)
-        result = [pool.apply_async(generate_resistance_matrices,
-                                   args=(e, n),
-                                   kwds={'a': a, 'b': b, 'domain': domain,
-                                         'distance': distance})
-                  for e in eps for n in n_nodes]
+        result = [
+            pool.apply_async(generate_resistance_matrices,
+                             args=(e, n),
+                             kwds={'a': a, 'b': b, 'domain': domain,
+                                   'distance': distance, 'shear_vec': False})
+            for e in eps for n in n_nodes
+        ]
         matrices = [res.get() for res in result]
 
     T = np.zeros(shape=(len(eps) * 3, len(n_nodes) * 3 + 1))
--- reg_stokeslets/3d/wall_adjacent_test.py
+++ reg_stokeslets/3d/wall_adjacent_test.py
@@ -23,6 +23,8 @@ def main(a=1., b=1., theta=0., phi=0., server='mac'):
     """
     true_par = np.array([1., 1.0591, 1.1738, 1.5675, 2.1515, 2.6475, 3.7863])
     true_prp = np.array([1., 1.1253, 1.4129, 3.0361, 9.2518, 23.6605, 201.864])
+    true_srf = np.array([1., 1.0587, 1.1671, 1.4391, 1.6160, 1.6682, 1.6969])
+    true_srt = np.array([1., 0.9998, 0.9997, 0.9742, 0.9537, 0.9477, 0.9444])
 
     if server == 'linux':
         n_nodes = np.array([8, 12, 16, 20])
@@ -49,6 +51,8 @@ def main(a=1., b=1., theta=0., phi=0., server='mac'):
 
     par_table = np.zeros(shape=(len(dist_ratios), 3 + len(n_nodes)))
     prp_table = np.zeros(shape=(len(dist_ratios), 3 + len(n_nodes)))
+    srf_table = np.zeros(shape=(len(dist_ratios), 3 + len(n_nodes)))
+    srt_table = np.zeros(shape=(len(dist_ratios), 3 + len(n_nodes)))
 
     par_table[:, 0] = dist_ratios
     par_table[:, 1] = distances - a
@@ -56,12 +60,22 @@ def main(a=1., b=1., theta=0., phi=0., server='mac'):
     prp_table[:, 0] = dist_ratios
     prp_table[:, 1] = distances - a
     prp_table[:, -1] = true_prp[:len(dist_ratios)]
+    srf_table[:, 0] = dist_ratios
+    srf_table[:, 1] = distances - a
+    srf_table[:, -1] = true_srf[:len(dist_ratios)]
+    srt_table[:, 0] = dist_ratios
+    srt_table[:, 1] = distances - a
+    srt_table[:, -1] = true_srt[:len(dist_ratios)]
 
     for (i, dist) in enumerate(distances):
         for (j, n) in enumerate(n_nodes):
             t_matrix = result[j][i][0]
+            shear_f = result[j][i][4]
+            shear_t = result[j][i][5]
             par_table[i, j + 2] = t_matrix[1, 1] / (6 * np.pi * a)
             prp_table[i, j + 2] = t_matrix[0, 0] / (6 * np.pi * a)
+            srf_table[i, j + 2] = shear_f[2] / (6 * np.pi * a * dist)
+            srt_table[i, j + 2] = shear_t[1] / (4 * np.pi * a**3)
 
     header = 'd/a, Gap size, '
     header += '{}, ' * len(n_nodes)
@@ -70,6 +84,8 @@ def main(a=1., b=1., theta=0., phi=0., server='mac'):
 
     np.savetxt('par_table.dat', par_table, delimiter=',', header=header)
     np.savetxt('prp_table.dat', prp_table, delimiter=',', header=header)
+    np.savetxt('srf_table.dat', srf_table, delimiter=',', header=header)
+    np.savetxt('srt_table.dat', srt_table, delimiter=',', header=header)
 
 
 if __name__ == '__main__':
