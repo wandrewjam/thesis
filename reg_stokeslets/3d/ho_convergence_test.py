f47482936db85c6a3fb6c00d3d2e464dc6c2f9a2
Andrew Watson <awatson@math.utah.edu>
2020-05-11 14:19:40 -0600

    Write convergence tests for position and orientation

--- reg_stokeslets/3d/ho_convergence_test.py
+++ reg_stokeslets/3d/ho_convergence_test.py
@@ -16,14 +16,14 @@ def spheroid_surface_area(a, b):
 def main(server='mac'):
     a, b = 1.5, 0.5
     surf_area = spheroid_surface_area(a, b)
-    c_list = [0.1, 0.2, 0.4, 0.6]
-    n_list = [0.8, 0.9, 1.]
 
-    distance_ratios = np.array([11013.2, 10.0677, 3.7622, 1.5431,
-                                1.1276, 1.0453, 1.005])
-    phi_list = np.linspace(0, np.pi / 2, num=9)
-    theta_list = np.linspace(0, np.pi, num=9)
-    h_list = distance_ratios * a
+    c, n = .4, .9
+
+    distance_ratios = np.array([10.0677, 3.7622, 1.5431,
+                                1.1276, 1.0453])
+    phi_list = np.linspace(0, np.pi / 2, num=3)
+    theta_list = np.linspace(0, np.pi, num=3)
+    d_list = distance_ratios * a
 
     # # For testing
     # c_list = [0.8, 1.]
@@ -39,32 +39,35 @@ def main(server='mac'):
 
     result_dict = {}
     exact_dict = {}
-    for c in c_list:
-        for n in n_list:
-            h = np.sqrt(surf_area / (6 * n_nodes ** 2 + 2))
-            epsilon = c * h ** n
-            result = [
-                (node,
-                 generate_resistance_matrices(e, node, a=a, b=b, domain='free',
-                                              shear_vec=True))
-                for (e, node) in zip(epsilon, n_nodes)
-            ]
+    for phi in phi_list:
+        for theta in theta_list:
+            for d in d_list:
+                h = np.sqrt(surf_area / (6 * n_nodes ** 2 + 2))
+                epsilon = c * h ** n
+                result = [
+                    (node,
+                     generate_resistance_matrices(
+                         e, node, a=a, b=b, domain='wall', shear_vec=True,
+                         distance=d, theta=theta, phi=phi)
+                     )
+                    for (e, node) in zip(epsilon, n_nodes)
+                ]
 
-            processed_result = list()
-            for node, matrices in result:
-                t, p, pt, r, s1, s2 = matrices
-                resistance_matrix = np.block([[t, p], [pt, r]])
-                shear_vec = np.concatenate([s1, s2], axis=0)
-                assert resistance_matrix.shape == (6, 6)
-                assert shear_vec.shape == (6, 1)
+                processed_result = list()
+                for node, matrices in result:
+                    t, p, pt, r, s1, s2 = matrices
+                    resistance_matrix = np.block([[t, p], [pt, r]])
+                    shear_vec = np.concatenate([s1, s2], axis=0)
+                    assert resistance_matrix.shape == (6, 6)
+                    assert shear_vec.shape == (6, 1)
 
-                processed_result.append((node, (resistance_matrix, shear_vec)))
+                    processed_result.append((node, (resistance_matrix, shear_vec)))
 
-            processed_result = dict(processed_result)
-            result_dict.update([((c, n), processed_result)])
-            exact_dict.update([((c, n), processed_result[n_max])])
+                processed_result = dict(processed_result)
+                result_dict.update([((phi, theta, d), processed_result)])
+                exact_dict.update([((phi, theta, d), processed_result[n_max])])
 
-    with open('ce_convergence.pkl', 'w') as f:
+    with open('ho_convergence.pkl', 'w') as f:
         pickle.dump(result_dict, f)
 
     errors = {}
@@ -79,7 +82,7 @@ def main(server='mac'):
 
         errors.update([(key, dict(err_sequence))])
 
-    with open('ce_errors.pkl', 'w') as f:
+    with open('ho_errors.pkl', 'w') as f:
         pickle.dump(errors, f)
 
     if server == 'mac':
@@ -88,7 +91,8 @@ def main(server='mac'):
             sorter = np.argsort(err.keys())
             jonathan, vals = np.array(err.keys()), np.array(err.values())
             jonathan_h = np.sqrt(surf_area / (6 * jonathan ** 2 + 2))
-            label_str = 'c = {}, n = {}'.format(key[0], key[1])
+            label_str = 'd = {}, $\\theta$ = {}, $\\phi$ = {}'.format(
+                key[2], key[1], key[0])
             ax.plot(jonathan_h[sorter], vals[sorter], label=label_str)
         ax.legend()
         ax.set_xlabel('Discretization size ($h$)')
