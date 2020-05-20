37e9fbf6e3ea7dbdbf249e33d12fef8ae545a752
Andrew Watson <awatson@math.utah.edu>
2020-05-20 12:03:30 -0600

    Add profiler to ho_convergence_test

--- reg_stokeslets/3d/ho_convergence_test.py
+++ reg_stokeslets/3d/ho_convergence_test.py
@@ -110,4 +110,5 @@ if __name__ == '__main__':
         os.environ["MKL_NUM_THREADS"] = num_threads
     import numpy as np
 
-    cProfile.run('main(server=sys.argv[1], proc=int(num_threads))')
+    cProfile.run('main(server=sys.argv[1], proc=int(num_threads))',
+                 filename='ho_conv_profile.stats')
