e6756d8b9a18aae26471102dd6f1cd331ed98033
Andrew Watson <awatson@math.utah.edu>
2020-06-01 14:27:27 -0600

    Add masked array test to symmetric test

--- reg_stokeslets/3d/symmetric_ops_test.py
+++ reg_stokeslets/3d/symmetric_ops_test.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     a = np.random.rand(size, size)
     ind_u = np.triu_indices(size, k=1)
     a2 = np.triu(a)
+    am = np.ma.array(a)
+    am[ind_u[::-1]] = np.ma.masked
 
     r = np.linspace(0, 1, num=100)
     sq = np.sqrt(r**2 + .01)
@@ -24,6 +26,14 @@ if __name__ == '__main__':
     print(end - start)
 
     assert np.all(res2 == res2.T)
+
+    start = timer()
+    res5 = np.sqrt(am**2 + .01)
+    a3 = np.zeros(shape=(size, size))
+    a3[ind_u] = am[ind_u]
+    a3[ind_u[::-1]] = am[ind_u]
+    end = timer()
+    print(end - start)
 
     start = timer()
     res3 = np.interp(a, r, sq)
