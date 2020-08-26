1fc28f54d42a13651bb1c8ee083dba798de1bf4b
Andrew Watson <awatson@math.utah.edu>
2020-08-26 11:43:37 -0600

    Add case for CHPC servers in test.sh

--- reg_stokeslets/3d/test.sh
+++ reg_stokeslets/3d/test.sh
@@ -4,16 +4,29 @@ if [ -z $1 ]; then
 fi
 
 osys=$(uname)
+name=$(uname -n)
 
 if [ $osys == "Darwin" ]; then
 	server="mac"
+	echo "On Mac; setting MKL threads"
 	export MKL_NUM_THREADS=1;
 elif [ $osys == "Linux" ]; then
 	server="linux"
-	export OPENBLAS_NUM_THREADS=1;
+	
+	if [[ $name =~ "peak" ]]; then
+		export OMP_NUM_THREADS=1;
+		echo "On CHPC server; setting OMP threads"
+	elif [[ $name =~ ".math.utah.edu" ]]; then
+		export OPENBLAS_NUM_THREADS=1;
+		echo "On Math server; setting OPENBLAS threads"
+	else
+		echo "uname -n returned an unexpected value"
+		exit 1;
+	fi
 else 
 	echo "uname returned an unexpected value"
 	exit 1;
 fi
 
-python motion_integration.py $1 $server
+# python motion_integration.py $1 $server
+echo $name
