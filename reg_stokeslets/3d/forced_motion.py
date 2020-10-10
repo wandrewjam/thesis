f972d31f190b2e58737295f926071e0cf355fcbc
Andrew Watson <awatson@math.utah.edu>
2020-10-10 12:43:36 -0600

    Change plotting in forced motion experiments

--- reg_stokeslets/3d/forced_motion.py
+++ reg_stokeslets/3d/forced_motion.py
@@ -20,7 +20,6 @@ def run_experiment(dist=0., a=1., b=1., exact_vels=None, proc=1,
         def exact_vels(em):
             return np.zeros(6)
 
-
     global n_nodes
     n_nodes = 16
 
@@ -217,32 +216,32 @@ def main():
         phi_fine = np.arccos(em_fine[0])
         theta_fine = np.arctan2(em_fine[2], em_fine[1])
 
-        # ax[0,1].plot(t, phi, t, theta, t, phi_fine, t, theta_fine)
-        # ax[0,1].legend(['$\\phi$ approx', '$\\theta$ approx',
-        #                 '$\\phi$ analytic', '$\\theta$ analytic'])
-        # ax[0,1].set_ylabel('Components of orientation vector')
-
-        ax[0,1].plot(t, e_m[0], t, e_m[1], t, e_m[2], t, em_fine[0],
-                     t, em_fine[1], t, em_fine[2])
-        ax[0,1].legend(['$e_x$ approx', '$e_y$ approx', '$e_z$ approx',
-                        '$e_x$ analytic', '$e_y$ analytic', '$e_z$ analytic'])
+        ax[0,1].plot(t, phi, t, theta, t, phi_fine, t, theta_fine)
+        ax[0,1].legend(['$\\phi$ approx', '$\\theta$ approx',
+                        '$\\phi$ analytic', '$\\theta$ analytic'])
         ax[0,1].set_ylabel('Components of orientation vector')
+
+        # ax[0,1].plot(t, e_m[0], t, e_m[1], t, e_m[2], t, em_fine[0],
+        #              t, em_fine[1], t, em_fine[2])
+        # ax[0,1].legend(['$e_x$ approx', '$e_y$ approx', '$e_z$ approx',
+        #                 '$e_x$ analytic', '$e_y$ analytic', '$e_z$ analytic'])
+        # ax[0,1].set_ylabel('Components of orientation vector')
 
         ax[1,0].plot(t, x2 - x_vec[1], t, x3 - x_vec[2])
         ax[1,0].legend(['$y$ error', '$z$ error'])
         ax[1,0].set_xlabel('t')
         ax[1,0].set_ylabel('CoM error')
 
-        # ax[1,1].plot(t, phi - phi_fine, t, theta - theta_fine)
-        # ax[1,1].legend(['$\\phi$ error', '$\\theta$ error'])
-        # ax[1,1].set_xlabel('t')
-        # ax[1,1].set_ylabel('Orientation error')
-
-        ax[1,1].plot(t, e_m[0] - em_fine[0], t, e_m[1] - em_fine[1],
-                     t, e_m[2] - em_fine[2])
-        ax[1,1].legend(['$e_x$ error', '$e_y$ error', '$e_z$ error'])
+        ax[1,1].plot(t, phi - phi_fine, t, theta - theta_fine)
+        ax[1,1].legend(['$\\phi$ error', '$\\theta$ error'])
         ax[1,1].set_xlabel('t')
         ax[1,1].set_ylabel('Orientation error')
+
+        # ax[1,1].plot(t, e_m[0] - em_fine[0], t, e_m[1] - em_fine[1],
+        #              t, e_m[2] - em_fine[2])
+        # ax[1,1].legend(['$e_x$ error', '$e_y$ error', '$e_z$ error'])
+        # ax[1,1].set_xlabel('t')
+        # ax[1,1].set_ylabel('Orientation error')
 
         # ax[1, 1].plot(t, np.linalg.norm(e_m, axis=0),
         #               t, np.linalg.norm(em_fine, axis=0),
@@ -252,9 +251,9 @@ def main():
         # ax[1, 1].set_ylabel('Norm')
 
         # ii = [3, 5]
-        plt.savefig('f_test{}_{}'.format(i, n_nodes), bbox_inches='tight')
-        # plt.tight_layout()
-        # plt.show()
+        # plt.savefig('f_test{}_{}'.format(i, n_nodes), bbox_inches='tight')
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == '__main__':
