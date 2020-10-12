fcb701ada1002b5df6b8f3e36539fab4436a3f11
Andrew Watson <awatson@math.utah.edu>
2020-10-12 11:49:33 -0600

    Modify profiling of .m scripts

--- reg_stokeslets/3d/quad_matrix_timer.m
+++ reg_stokeslets/3d/quad_matrix_timer.m
@@ -1,6 +1,8 @@
+profile on
 clear variables
 load('quad_matrix.mat')
 s_matrix = s_matrix + triu(s_matrix, 1)';
 intermediate_solve = s_matrix\rhs;
 pt_forces = intermediate_solve ./ repmat(repelem(weights, 3)', 1, 7);
 pt_forces = reshape(pt_forces, [], 3, 7);
+profsave(profile('info'), 'quad_matrix_results')
