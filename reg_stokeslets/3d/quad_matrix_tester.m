2248c8a05a36270fade89ae6bf7eebe83fdc98bc
Andrew Watson <awatson@math.utah.edu>
2020-10-12 10:56:08 -0600

    Write timing functions in MATLAB

---
+++ reg_stokeslets/3d/quad_matrix_tester.m
@@ -0,0 +1,6 @@
+clear variables
+load('quad_matrix.mat')
+s_matrix = s_matrix + triu(s_matrix, 1)';
+intermediate_solve = s_matrix\rhs;
+pt_forces = intermediate_solve ./ repmat(repelem(weights, 3)', 1, 7);
+pt_forces = reshape(pt_forces, [], 3, 7);
