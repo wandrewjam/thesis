f7a790ba07e070c5bea1a00a57cb5d14b5cb108f
Andrew Watson <awatson@math.utah.edu>
2020-11-30 12:09:51 -0700

    Save data from binding experiments

--- reg_stokeslets/3d/binding_expt.py
+++ reg_stokeslets/3d/binding_expt.py
@@ -7,7 +7,28 @@ import os
 from scipy.io import savemat
 
 
-if __name__ == '__main__':
+def save_info(fname, seed, t_span, num_steps, n_nodes, a, b, adaptive, shear,
+              l_sep, dimk0_on, dimk0_off, sig, sig_ts):
+    with open(fname, 'w') as f:
+        expt_info = ['seed, {}\n'.format(seed),
+                     't_span, [{} {}]\n'.format(*t_span),
+                     'num_steps, {}\n'.format(num_steps),
+                     'n_nodes, {}\n'.format(n_nodes),
+                     'a, {}\n'.format(a),
+                     'b, {}\n'.format(b),
+                     'adaptive, {}\n'.format(adaptive),
+                     'shear, {}\n'.format(shear),
+                     'l_sep, {}\n'.format(l_sep),
+                     'dimk0_on, {}\n'.format(dimk0_on),
+                     'dimk0_off, {}\n'.format(dimk0_off),
+                     'sig, {}'.format(sig),
+                     'sig_ts, {}'.format(sig_ts)
+                     ]
+
+        f.writelines(expt_info)
+
+
+def main():
     save_data = True
     plot_data = True
     save_dir = 'data/'
@@ -17,10 +38,12 @@ if __name__ == '__main__':
         max_i = max(file_i)
     except ValueError:
         max_i = -1
-
     expt = 3
     t_span = [0., 50.]
     num_steps = 250
+
+    seed = None
+    np.random.seed(seed)
 
     def exact_vels(em):
         return np.zeros(6)
@@ -29,7 +52,6 @@ if __name__ == '__main__':
     a, b = 1.5, .5
     domain = 'wall'
     adaptive = False
-
     if expt == 1:
         bonds = np.array([[0, 0., 0.]])
         init = np.array([.6, 0, 0, 1., 0, 0])
@@ -46,16 +68,24 @@ if __name__ == '__main__':
     else:
         raise ValueError('expt is not valid')
 
+    # Define parameters
+    shear = 100.
+    l_sep = .1
+    dimk0_on = 1000.
+    dimk0_off = 5.
+    sig = 1e2
+    sig_ts = 9.99e1
     t_sc, f_sc, lam, k0_on, k0_off, eta, eta_ts, kappa = nondimensionalize(
-        l_scale=1, shear=100., mu=4e-3, l_sep=.1, dimk0_on=10., dimk0_off=5.,
-        sig=1e2, sig_ts=9.99e1, temp=310.)
+        l_scale=1, shear=shear, mu=4e-3, l_sep=l_sep, dimk0_on=dimk0_on,
+        dimk0_off=dimk0_off, sig=sig, sig_ts=sig_ts, temp=310.)
 
     result = integrate_motion(
         t_span, num_steps, init, exact_vels, n_nodes, a, b, domain,
         adaptive=adaptive, receptors=receptors, bonds=bonds, eta=eta,
         eta_ts=eta_ts, kappa=kappa, lam=lam, k0_on=k0_on, k0_off=k0_off)
 
-    t = np.linspace(t_span[0], t_span[1], num_steps + 1) * t_sc
+    # t = result[9] * t_sc
+    t = np.linspace(t_span[0], t_span[1], num=num_steps+1) * t_sc
     mask = result[0] > 0
 
     if plot_data:
@@ -75,8 +105,8 @@ if __name__ == '__main__':
         center = np.stack(result[:3])
         # fig, ax = plt.subplots()
         # if expt == 3:
-            # Figure out how to plot bonds individually (and only bonds that exist at a particular time point)
-            # Use a dict to track individual bonds over time
+        # Figure out how to plot bonds individually (and only bonds that exist at a particular time point)
+        # Use a dict to track individual bonds over time
         bond_dict = {}
         for i, t_i in enumerate(t):
             rmat = result[3][:, :, i]
@@ -107,20 +137,38 @@ if __name__ == '__main__':
 
     if save_data:
         new_i = str(max_i + 1)
-        new_i = '0'*(3-len(new_i)) + new_i
+        new_i = '0' * (3 - len(new_i)) + new_i
 
+        file_fmt = 'bd_expt{}'.format(new_i)
+
+        x, y, z, r_matrices = result[:4]
+        bond_history = result[8]
+
+        max_bonds = len(max(*bond_history, key=len))
+        padded_bonds = [np.pad(bd, pad_width=((0, max_bonds - bd.shape[0]),
+                                              (0, 0)),
+                               mode='constant', constant_values=-1)
+                        for bd in bond_history]
+        bond_array = np.stack(padded_bonds, axis=-1)
+
+        np.savez(file_fmt, t, x, y, z, r_matrices, bond_array, receptors, t=t,
+                 x=x, y=y, z=z, r_matrices=r_matrices, bond_array=bond_array,
+                 receptors=receptors)
+        fname = 'info_' + file_fmt + '.txt'
+        save_info(fname, seed, t_span, num_steps, n_nodes, a, b, adaptive,
+                  shear, l_sep, dimk0_on, dimk0_off, sig, sig_ts)
 
     bond_num = [bond.shape[0] for bond in result[-1]]
     print(bond_num)
-
     theta = np.arctan2(result[3][2, 0, mask][-1], result[3][1, 0, mask][-1])
     phi = np.arccos(result[3][0, 0, mask][-1])
-
     eps = eps_picker(n_nodes, a=a, b=b)
     shear_f, shear_t = generate_resistance_matrices(
         eps, n_nodes, a=a, b=b, domain=domain, distance=center[0, mask][-1],
         theta=theta, phi=phi)[-2:]
-
     print(np.stack([shear_f, shear_t]))
-
     print('Done!')
+
+
+if __name__ == '__main__':
+    main()
