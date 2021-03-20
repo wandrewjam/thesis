c53a0aafb329e1377b33325e8b2225258e016e84
Andrew Watson <awatson@math.utah.edu>
2021-03-20 12:05:39 -0600

    Add files from running rolling experiments

---
+++ reg_stokeslets/src/scripts/test03.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+cd $HOME/thesis/reg_stokeslets/
+
+module unload miniconda3/latest
+module load miniconda3/latest
+conda activate rolling
+
+DATA_PATH="data/bd_run/"
+SUFFIX=".npz"
+LINES=$(cat par-files/bd_runner1103.txt)
+
+for LINE in $LINES
+do
+    if [ -f "$DATA_PATH$LINE$SUFFIX" ]; then
+	continue
+    fi
+    echo $LINE >> tmpfile03.txt
+done
